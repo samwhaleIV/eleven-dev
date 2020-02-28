380becf068717d2ed568a16f598d1166d3abb95c
Samuel Robinson <pinkstarsr11@gmail.com>
2020-02-27 19:35:41 -0800

    Support grid custom base sizes

--- grid2d-test/script.js
+++ grid2d-test/script.js
@@ -1,12 +1,16 @@
-function World() {
-    const grid = new Eleven.Grid2D();
+function World(optimized=false) {
+    const grid = new Eleven.Grid2D(optimized?256:16);
     const tileRenderer = new Eleven.TileRenderer(grid,{
-        width: 16, height: 16, layers: 1
+        width: optimized?1:16, height: optimized?1:16, layers: 1
     });
     grid.renderer = tileRenderer;
 
     tileRenderer.renderData.forEach(layer => {
-        for(let i = 0;i<layer.length;i++) layer[i] = i;
+        if(optimized) {
+            for(let i = 0;i<layer.length;i++) layer[i] = 0;
+        } else {
+            for(let i = 0;i<layer.length;i++) layer[i] = i;
+        }
     });
 
     const camera = grid.camera.center();
