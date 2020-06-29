5887050075776346e3a660e1d763dba8501962b7
Samuel Robinson <pinkstarsr11@gmail.com>
2020-06-28 22:44:14 -0700

    New tileset sprites

--- seven-circles/untiled/scripts/floor-plan-matrices.js
+++ seven-circles/untiled/scripts/floor-plan-matrices.js
@@ -1,44 +1,46 @@
 const getOuterWallPattern = wall => {
+    const {full,inverse} = wall;
     return {
         /* Simple walls */
-        "*** fw* ***": wall.full[1][0], //left
-        "*** *wf ***": wall.full[1][2], //Right
-        "*** *w* *f*": wall.full[2][1], //Top
-        "*f* *w* ***": wall.full[0][1], //Bottom
+        "*** fw* ***": full[1][0], //left
+        "*** *wf ***": full[1][2], //Right
+        "*** *w* *f*": full[2][1], //Top
+        "*f* *w* ***": full[0][1], //Bottom
 
         /* Corner pieces */
-        "*w* wwf ***": wall.full[2][2], //Top left
-        "*w* fww ***": wall.full[2][0], //Top right
-        "*** wwf *w*": wall.full[0][2], //Bottom left
-        "*** fww *w*": wall.full[0][0], //Bottom right
+        "*w* wwf ***": full[2][2], //Top left
+        "*w* fww ***": full[2][0], //Top right
+        "*** wwf *w*": full[0][2], //Bottom left
+        "*** fww *w*": full[0][0], //Bottom right
 
         /* Inverse corners */
-        "*** *ww *w*": wall.inverse[0][0], //Top left
-        "*w* *ww ***": wall.inverse[1][0], //Top right
-        "*** ww* *w*": wall.inverse[0][1], //Bottom left
-        "*w* ww* ***": wall.inverse[1][1] //Bottom right
+        "*** *ww *w*": inverse[0][0], //Top left
+        "*w* *ww ***": inverse[1][0], //Top right
+        "*** ww* *w*": inverse[0][1], //Bottom left
+        "*w* ww* ***": inverse[1][1] //Bottom right
     };
 };
 
 const getInnerWallPattern = wall => {
+    const {full,inverse} = wall;
     return {
         /* Simple walls */
-        "*** wf* ***": wall.full[1][0], //Left
-        "*** *fw ***": wall.full[1][2], //Right
-        "*** *f* *w*": wall.full[2][1], //Top
-        "*w* *f* ***": wall.full[0][1], //Bottom
+        "*** wf* ***": full[1][0], //Left
+        "*** *fw ***": full[1][2], //Right
+        "*** *f* *w*": full[2][1], //Top
+        "*w* *f* ***": full[0][1], //Bottom
 
         /* Corner pieces */
-        "ww* wf* ***": wall.full[0][0], //Top left
-        "*ww *fw ***": wall.full[0][2], //Top right
-        "*** wf* ww*": wall.full[2][0], //Bottom left
-        "*** *fw *ww": wall.full[2][2], //Bottom right
+        "ww* wf* ***": full[0][0], //Top left
+        "*ww *fw ***": full[0][2], //Top right
+        "*** wf* ww*": full[2][0], //Bottom left
+        "*** *fw *ww": full[2][2], //Bottom right
 
         /* Inverse corners */
-        "wf* ff* ***": wall.inverse[1][1], //Top left
-        "*fw *ff ***": wall.inverse[1][0], //Top right
-        "*** ff* wf*": wall.inverse[0][1], //Bottom left
-        "*** *ff *fw": wall.inverse[0][0]  //Bottom right
+        "wf* ff* ***": inverse[1][1], //Top left
+        "*fw *ff ***": inverse[1][0], //Top right
+        "*** ff* wf*": inverse[0][1], //Bottom left
+        "*** *ff *fw": inverse[0][0]  //Bottom right
     };
 };
 
