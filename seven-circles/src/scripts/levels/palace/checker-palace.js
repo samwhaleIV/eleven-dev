4927978223b9b834b141143b1f5f5f1dedc2f993
Samuel Robinson <pinkstarsr11@gmail.com>
2020-06-30 01:10:09 -0700

    Add the terrifying hand of doom

--- seven-circles/src/scripts/levels/palace/checker-palace.js
+++ seven-circles/src/scripts/levels/palace/checker-palace.js
@@ -2,8 +2,9 @@ import {AddPalaceFloor} from "../helper.js";
 
 function CheckerPalace({world}) {
     world.setMap("checker-palace");
-    world.addPlayer(10,5,"down");
+    world.addPlayer(9,3.5,"down");
     AddPalaceFloor(world);
+    world.camera.verticalPadding = true;
 }
 export default CheckerPalace;
 
